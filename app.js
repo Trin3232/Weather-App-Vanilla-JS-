377856bd931fbc798414e9880592501b70a80844
Trin3232 <emanuel6464@mail.com>
2019-04-05 17:17:41 +0100

    F to C conversion & other tweaks

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ window.addEventListener('load', ()=> {
 	let lat;
 	let long; 
 
-	let timezonelocation = document.querySelector(".timezone-location");
-	let tempdescrip = document.querySelector(".temp-descrip");
-	let tempdegree = document.querySelector(".temp-degree");
+	const timezonelocation = document.querySelector(".timezone-location");
+	const tempdescrip = document.querySelector(".temp-descrip");
+	const tempsection = document.querySelector(".temp-section");
+	const tempspan = document.querySelector(".temp-section span");
+	const tempdegree = document.querySelector(".temp-degree");
 
 	if(navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(locate => {
@@ -20,16 +22,48 @@ window.addEventListener('load', ()=> {
 				})
 				.then(data => {
 					console.log(data);
-					const { temperature } = data.currently;
+					const { temperature, icon } = data.currently;
 					const { summary } = data.minutely;
 					// Elements for the API
 					
 					timezonelocation.textContent = data.timezone;
 					tempdescrip.textContent = summary;
 					tempdegree.textContent  = temperature;
+					 
+					// Set Icons
+					
+					setIcons(icon, document.querySelector(".icon"));
+
+					//  Temperature F to C formula
+
+					let celsius = (temperature -32) * (5 / 9);
+
+					// Change Temp from F to C....
+					
+					tempsection.addEventListener("click", () => {
+					  if (tempspan.textContent === "F") {
+							 tempspan.textContent = "C";
+							 tempdegree.textContent = Math.floor(celsius);
+						} else { 
+							tempspan.textContent = "F";
+							tempdegree.textContent = temperature;
+						}
+					});
 			  });
 		});
-	}	
+	}
+
+	// Get the id of the canvas & what type of icon to display
+
+	function setIcons( icon, iconID ) {
+		const skycons = new Skycons({color: "white" });
+
+		// We are replacing ever line with and underscore instead when getting icons & making it uppercase
+
+		const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+		skycons.play();
+		return skycons.set(iconID, Skycons[currentIcon]);
+	}
 });
 
 /*else{
